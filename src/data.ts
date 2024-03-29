14d3d6751f35a89f1609feaf36aa1e4564c33b82
Luzin7 <lvictor.dutra@gmail.com>
2024-03-28 23:14:36 -0300

    feat: create fakejobs

---
+++ src/data.ts
@@ -0,0 +1,18 @@
+import { Job } from './types/job';
+
+export const fakeJobs: Job[] = [
+  {
+    id: '123',
+    createdAt: new Date(),
+    title: 'title',
+    description: 'description',
+    link: 'link',
+  },
+  {
+    id: '456',
+    createdAt: new Date(),
+    title: 'title',
+    description: 'description',
+    link: 'link',
+  },
+];
