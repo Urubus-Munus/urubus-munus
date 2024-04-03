6be48155c48a29c1d1467fe3e0a61ff420e610e4
BritoRuan <ruan.desenvolvedor@gmail.com>
2024-04-03 02:11:04 -0300

    fix: folder name correction.

--- src/infra/implementations/jobs/index.ts
+++ src/infra/implementations/jobs/index.ts
@@ -1,4 +1,4 @@
-import { JobsImplementations } from '@/modules/jobs/repositories/implemetations/Jobs.implementations';
+import { JobsImplementations } from '@/modules/jobs/repositories/implementations/Jobs.implementations';
 import { FindAllJobs } from '@/modules/jobs/useCases/fetchAllJobs/fetchAllJobs.useCase';
 
 export const findAllJobsUseCase = new FindAllJobs(new JobsImplementations());
