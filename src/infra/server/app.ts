1a57e45168b897ea1cd7b4fd9c140442234bbf46
BritoRuan <ruan.desenvolvedor@gmail.com>
2024-04-03 02:12:58 -0300

    refactor: code improvement.

--- src/infra/server/app.ts
+++ src/infra/server/app.ts
@@ -1,44 +1,6 @@
 import { env } from '@/env';
-import { DMChannel, TextChannel, hyperlink } from 'discord.js';
+import { findAllJobs } from '@/functions/findAllJobs';
 import { discordClient } from '../discordClient/config';
-import { findAllJobsUseCase } from '../implementations/jobs';
-
-// MELHORA ESSA FUNCAO
-async function test() {
-  const jobs = await findAllJobsUseCase.execute();
-  const response = jobs.value?.jobs;
-
-  if (!response) {
-    return;
-  }
-
-  const isDevelopment: boolean = env.NODE_ENV === 'dev';
-  const channel = discordClient.channels.cache.get(
-    isDevelopment
-      ? env.ID_DISCORD_TEST_SERVER_CHANNEL
-      : env.ID_DISCORD_SERVER_CHANNEL,
-  );
-  const bodyContent = response.map(
-    ({ createdAt, description, id, link, title }) => {
-      return `
-VAGUINHA ${title}:
-
-ACHEI EM ${createdAt.toLocaleString()}, DESSRIÇÃ DA VAGA: ${description}, ${hyperlink('ACESSE AQUI', link)}
-
-
-${id}
-    `;
-    },
-  );
-
-  const fodase = `${bodyContent}`;
-  if (
-    (channel && channel instanceof TextChannel) ||
-    channel instanceof DMChannel
-  ) {
-    channel.send(fodase);
-  }
-}
 
 discordClient.login(env.TOKEN_BOT);
 
@@ -46,5 +8,5 @@ discordClient.on('ready', async () => {
   console.log(
     `Bot started, now connected to Discord server as ${discordClient.user?.tag}`,
   );
-  test();
+  findAllJobs();
 });
