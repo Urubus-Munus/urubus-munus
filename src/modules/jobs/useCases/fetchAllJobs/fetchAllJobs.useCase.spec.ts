567c9424d0f0aee27335bf42be07c38b7296eceb
Luzin7 <lvictor.dutra@gmail.com>
2024-04-04 02:20:27 -0300

    test: add test case for find all jobs use case

---
+++ src/modules/jobs/useCases/fetchAllJobs/fetchAllJobs.useCase.spec.ts
@@ -0,0 +1,21 @@
+import { InMemoryJobsImplementations } from '@/tests/modules/jobs/repositories/inMemoryJobsRepository';
+import { error } from 'console';
+import { describe, expect, it, onTestFailed } from 'vitest';
+import { FindAllJobs } from './fetchAllJobs.useCase';
+
+describe('Fetch All Jobs Use Case', () => {
+  const jobsRepository = new InMemoryJobsImplementations();
+  const sut = new FindAllJobs(jobsRepository);
+
+  it('Should return an array of jobs', async () => {
+    const response = await sut.execute();
+
+    if (response.isRight()) {
+      expect(response.value.jobs).toBeDefined();
+      expect(response.value.jobs.length).toBeGreaterThan(0);
+      expect(response.value.jobs[0]).toHaveProperty('id');
+    } else {
+      onTestFailed(error);
+    }
+  });
+});
